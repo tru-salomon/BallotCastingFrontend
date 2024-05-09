47542ded9401f8843dd0edbb1e5c35d7addc5e97
Salomon Balthazar <131703377+tru-salomon@users.noreply.github.com>
2024-05-08 22:57:49 -0400

    login page completed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,36 +1,33 @@
 import background from '../assets/01 1.png'
+import logo from '../assets/BCS LOGO.png'
 
 const login = () => {
-  return (
-      <div style={{
-          backgroundImage: `url(${background})`,
-          backgroundSize: 'cover',
-          height: '100vh',
-          width: '100vw',
-          display: 'flex',
-          justifyContent: 'center',
-            alignItems: 'center',
-      }}>
-          <form style={{
-              display: 'flex',
-              flexDirection: 'column',
-              gap: '10px',
-              backgroundColor: 'white',
-              padding: '20px',
-              borderRadius: '32px',
-              width: '300px', // Adjust as needed
-          }}>
-              <input type="email" placeholder="Email" required autoComplete='email' />
-              <input type="password" placeholder="Password" required autoComplete='current-password'/>
-              <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <input type="checkbox" id="rememberMe" name="rememberMe" />
-                  <label htmlFor="rememberMe">Remember me</label>
-              </div>
-              <a href="/forgot-password" style={{ alignSelf: 'flex-end' }}>Forgot password?</a>
-              <button type="submit">Sign In</button>
-          </form>
-    </div>
-  )
+    return (
+        <div className="login-background" style={{ backgroundImage: `url(${background})` }}>
+            <form className="login-form">
+                <img src={logo} alt="logo" className='logo' />
+                <div className="login-title">
+                    <h2>Welcome</h2>
+                    <p>Sign in to continue</p>
+                </div>
+                <div className='input-group'>
+                    <input type="email" placeholder="Enter your email" required autoComplete="email" />
+                    <input type="password" placeholder="**********" required autoComplete="current-password" />
+                </div>
+                <div className="remember-me-forgot-password">
+                    <div className="remember-me">
+                        <input type="checkbox" id="rememberMe" name="rememberMe" />
+                        <label htmlFor="rememberMe">Remember me</label>
+                    </div>
+                    <a href="/forgot-password" className="forgot-password">Forgot password?</a>
+                </div>
+                <button type="submit">Sign In</button>
+                <div className='signup'>
+                    <p>Don't have an account? <a href="/signup">Request an account here!</a></p>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default login
