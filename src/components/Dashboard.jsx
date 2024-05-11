7f6c161a6c84ab564979d9ceb17316a2eb6923ea
Salomon Balthazar <131703377+tru-salomon@users.noreply.github.com>
2024-05-11 19:39:56 -0400

    responsive navbar

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,18 @@
-import logo from '../assets/BCS LOGO.png'; // adjust the path to match your project structure
+import { useState } from 'react';
+import logo from '../assets/BCS LOGO.png'; 
 import './Dashboard.css';
 
 const Dashboard = () => {
+  const [activeItem, setActiveItem] = useState('/dashboard')
+  
+  const handleNavItemClick = (path) => {
+    setActiveItem(path);
+  }
+
+  const handleNewCallClick = () => {
+    console.log("you started a new call")
+  }
+
   return (
 
     <div className="dashboard-container">
@@ -12,23 +23,28 @@ const Dashboard = () => {
           <div className='sidebar-nav-items'>
             <div className='nav-item'>
               <input type="text" placeholder="start new call" />
-              <i className="fa fa-fw fa-plus"></i>
+             <a href="/newcall"><i className="fa fa-fw fa-plus"></i></a>
             </div>
-            <div className='nav-item'>
+            <div className={`nav-item ${activeItem === '/dashboard' ? 'active' : ''}`} onClick={() => handleNavItemClick('/dashboard')}>
               <a href="/dashboard"><i className="fa fa-fw fa-home"></i>Dashboard</a>
             </div>
-            <div className='nav-item'>
+            <div className={`nav-item ${activeItem === '/analytics' ? 'active' : ''}`} onClick={() => handleNavItemClick('/analytics')}>
               <a href="/analytics"><i className="fa fa-fw fa-bar-chart"></i>Analytics</a>
             </div>
-            <div className='nav-item'>
+            <div className={`nav-item ${activeItem === '/pools' ? 'active' : ''}`} onClick={() => handleNavItemClick('/pools')}>
               <a href="/pools"><i className="fa fa-fw fa-users"></i>Pool Groups</a>
             </div>
-            <div className='nav-item'>
+            <div className={`nav-item ${activeItem === '/ballots' ? 'active' : ''}`} onClick={() => handleNavItemClick('/ballots')}>
               <a href="/ballots"><i className="fa fa-fw fa-envelope"></i>Ballots</a>
               </div>
-                <div className='nav-item'>
+            <div className={`nav-item ${activeItem === '/archive' ? 'active' : ''}`} onClick={() => handleNavItemClick('/analytics')}>
               <a href="/archives"><i className="fa fa-fw fa-archive"></i>Archives</a>
-                </div>
+            </div>
+            <div className='lower-sidebar'>
+              <div className='nav-item'>
+                <a href='/settings'><i className='fa fw fa-gear' ></i>Settings</a>
+              </div>
+              </div>
               </div>
             </div>
           </div>
