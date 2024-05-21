7681807ebe5520d7e92cde85046e7ca6c9055208
Salomon Balthazar <131703377+tru-salomon@users.noreply.github.com>
2024-05-21 14:02:19 -0400

    dashboard layout completed

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,15 +1,43 @@
 import './Dashboard.css';
 
 const Dashboard = () => {
-  return (
+      return (
 
-        <div className="dashboard-container">
-              <div className='dashboard'>
-                    
-              </div>
-          <div>Dashboard</div>
-          </div>
+            <div className="dashboard-container">
+                  <div className='dashboard'>
+                        <div className='title-container'>Overview</div>
+                        <div className='data-visualization-container'>
+                              <div className='data-visualization'>
+                                    <div className='bar-chart-container'>
+                                          <div className='chart-title'>Ballots</div>
+                                          <div className='chart-container'></div>
+                                    </div>
+
+                                    <div className='pie-chart-container'>
+                                          <div className='chart-title'>Turnout</div>
+                                          <div className='chart-container'></div>
+                                    </div>
+                              </div>
+                        </div>
+                        <div className='dashboard-feed-container'>
+
+                              <div className='result-feed-container'>
+                                    <div className='feed-title'>Current Call Preliminary Results</div>
+                                    <div className='result-container'>
+                                          <div className='cell-title-container'></div>
+                                    </div>
+
+                              </div>
+                              <div className='activity-feed-container'>
+                                    <div className='feed-title'>Activity Feed</div>
+                                    <div className='activity-container'></div>
+                              </div>
+
+                        </div>
+                  </div>
+
+            </div>
       )
 }
 
-      export default Dashboard
+export default Dashboard
